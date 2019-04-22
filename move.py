179e28eefeb2e441ece8756449115807cd59a2cb
Blake-Latchford <blake.latchford@gmail.com>
2019-04-21 20:16:43 -0400

    Test _creates_ring_mill

--- move.py
+++ move.py
@@ -1,16 +1,14 @@
 
-
-
 class Move:
     def __init__(self, board, target, source=None):
         self.board = board
         self.target = (
             target[0] % board.num_rings,
-            target[1] % board.num_rings)
+            target[1] % board.ring_size)
         if source:
             self.source = (
                 source[0] % board.num_rings,
-                source[1] % board.num_rings)
+                source[1] % board.ring_size)
         else:
             self.source = None
 
@@ -34,6 +32,8 @@ class Move:
             return False
         if self._creates_spoke_mill():
             return True
+        if self._creates_ring_mill():
+            return True
 
         return self._creates_ring_mill()
 
@@ -55,27 +55,34 @@ class Move:
         return False
 
     def _creates_ring_mill(self):
-        if not self.source:
-            return False
-
         ring = self.board.rings[self.target[0]]
 
         break_interval = max(self.board.spoke_period, 2)
 
         # If target[1] is on a corner, then start != mid != end
-        # Otherwise we'll duplicate some work.
-        start = ((self.target[1] - 1) // break_interval) * break_interval
+        # Otherwise duplicate some work.
+        start = (self.target[1] - 1) % self.board.ring_size
+        start = (start // break_interval) * break_interval
         mid = (self.target[1] // break_interval) * break_interval
         end = ((self.target[1] + 1) // break_interval) * break_interval
 
+        index = start
+        if index < mid:
+            while index <= mid:
+                if index != self.target[1] and ring[index] != self.board.turn:
+                    break
+                index += 1
+            else:
+                return True
 
-        adjacent_count = 1
-        for offset in range(1, 3):
-            position = self.target[1] + offset
-            if ring[position] != self.board.turn:
-                break
-            if self.source and self.source[0] == self.target[0] and \
-                    self.source[1] == position:
-                break
+        if index < end:
+            while index <= end:
+                index_mod = index % self.board.ring_size
+                if index_mod != self.target[1] and \
+                    ring[index_mod] != self.board.turn:
+                    break
+                index += 1
+            else:
+                return True
 
-        return adjacent_count > 3
+        return False
--- move_test.py
+++ move_test.py
@@ -29,9 +29,20 @@ class TestOperations(unittest.TestCase):
         self.assertFalse(
             Move(board, (0, 0)).is_valid())
 
+    def test_creates_mill_invalid_move(self):
+        board = Board()
+        board.rings[0][0] = Board.Player.white
+
+        self.assertFalse(
+            Move(board, (0, 0)).creates_mill())
+
     def test_creates_spoke_mill(self):
         board = Board()
         board.rings[0][1] = Board.Player.white
+
+        self.assertFalse(
+            Move(board, (1, 1)).creates_mill())
+
         board.rings[2][1] = Board.Player.white
 
         self.assertTrue(
@@ -42,9 +53,45 @@ class TestOperations(unittest.TestCase):
         board.rings[1][1] = Board.Player.white
 
         self.assertFalse(
-            Move(board, (1, 1)).creates_mill())
+            Move(board, (0, 0)).creates_mill())
+
+    def test_creates_ring_mill_upper(self):
+        board = Board()
+        board.rings[0][1] = Board.Player.white
+
         self.assertFalse(
             Move(board, (0, 0)).creates_mill())
+
+        board.rings[0][2] = Board.Player.white
+
+        self.assertTrue(
+            Move(board, (0, 0)).creates_mill())
+        self.assertFalse(
+            Move(board, (0, -1)).creates_mill())
+
+        board.rings[0][3] = Board.Player.white
+
+        self.assertTrue(
+            Move(board, (0, 4)).creates_mill())
+        self.assertFalse(
+            Move(board, (0, -1)).creates_mill())
+
+    def test_creates_ring_mill_lower(self):
+        board = Board()
+        board.rings[0][6] = Board.Player.none
+        
+        self.assertFalse(
+            Move(board, (0, 0)).creates_mill())
+        self.assertFalse(
+            Move(board, (0, 7)).creates_mill())
+        
+        board.rings[0][7] = Board.Player.white
+
+        self.assertTrue(
+            Move(board, (0, -1)).creates_mill())
+        self.assertFalse(
+            Move(board, (1, -1)).creates_mill())
+
 
 if __name__ == '__main__':
     unittest.main(exit=False)
